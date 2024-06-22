510e8530e7eb337c3a965aa3978c3089bebe64bc
Jorge Ramos <jorgeramos2068@gmail.com>
2024-06-21 18:04:29 -0600

    feat: add and configure accordion component

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,40 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+
+interface Item {
+  id: string;
+  question: string;
+  answer: string;
+}
+
+const items: Item[] = [
+  {
+    id: 'item-1',
+    question: 'Is it accessible?',
+    answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+  },
+  {
+    id: 'item-2',
+    question: 'Is it styled?',
+    answer: "Yes. It comes with default styles that matches the other components' aesthetic.",
+  },
+  {
+    id: 'item-1',
+    question: 'Is it animated?',
+    answer: "Yes. It's animated by default, but you can disable it if you prefer.",
+  },
+];
+
 export default function Page() {
   return (
     <div>
-      <h1>Accordion Page</h1>
+      <Accordion type="multiple" className="w-full">
+        {items.map(({ id, question, answer }) => (
+          <AccordionItem value={id} key={id}>
+            <AccordionTrigger>{question}</AccordionTrigger>
+            <AccordionContent>{answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 }
