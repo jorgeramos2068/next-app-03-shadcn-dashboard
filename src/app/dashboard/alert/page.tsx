4dad23b2b3d833df54edf451bd96f634b61b566e
Jorge Ramos <jorgeramos2068@gmail.com>
2024-06-21 18:14:24 -0600

    feat: add and configure alert component

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,7 +1,26 @@
+import { RocketIcon } from '@radix-ui/react-icons';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className="grid gap-3">
+      <Alert>
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>You can add components to your app using the cli.</AlertDescription>
+      </Alert>
+
+      <Alert variant="destructive">
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>You can add components to your app using the cli.</AlertDescription>
+      </Alert>
+
+      <Alert variant="success">
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>You can add components to your app using the cli.</AlertDescription>
+      </Alert>
     </div>
   );
 }
