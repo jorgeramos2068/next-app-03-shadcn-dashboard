33b338e32f3a98d681ea45dacd76828fef1f87f7
Jorge Ramos <jorgeramos2068@gmail.com>
2024-06-29 12:01:23 -0600

    feat: add and configure skeleton component

---
+++ src/app/dashboard/skeleton/page.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+
+const getData = async () => {
+  await new Promise(resolve => setTimeout(resolve, 3000));
+  return '123456789'.split('');
+};
+
+export default async function Page() {
+  const data = await getData();
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+      {data.map(item => (
+        <Card key={item}>
+          <CardHeader className="flex flex-row">
+            <Image
+              src="https://github.com/shadcn.png"
+              alt="Shadcn"
+              width={40}
+              height={40}
+              className="rounded-full mr-2 w-10 h-10"
+            />
+            <div>
+              <CardTitle>Title</CardTitle>
+              <CardDescription>Description</CardDescription>
+            </div>
+          </CardHeader>
+          <CardFooter className="flex justify-end">
+            <Button>More</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
